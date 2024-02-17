563ec9bc91ee7a7388586e0458b3cf28aebebc11
ShivangiPatel102 <patelshivangi2002@gmail.com>
2024-02-17 19:39:03 +0530

    ui h=changes in SmallCard , in home_screen and pta_test_screen.dart

--- lib/constants.dart
+++ lib/constants.dart
@@ -8,6 +8,7 @@ const kNavyBlueColor = Color(0xff28334A);
 // const kWhiteBgColor = Color(0xffF8F8F8);
 const kWhiteBgColor = Color(0xffEFEEEE);
 const kWhiteWidgetColor = Color(0xfffefcfc);
+const kVeryLightGreyColor = Color(0xffF6F4F4);
 
 // textstyles
 const kTitleOfAppBarTextStyle = TextStyle(
--- lib/screens/pta_test_screen.dart
+++ lib/screens/pta_test_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:freq_fit/constants.dart';
 import 'package:freq_fit/widgets/my_drawer.dart';
 import 'package:freq_fit/widgets/app_bar.dart';
 import 'package:freq_fit/widgets/card.dart';
@@ -9,77 +10,128 @@ class PtaTestScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      //backgroundColor: const Color.fromRGBO(248, 248, 248, 1),
+      resizeToAvoidBottomInset: false,
       appBar: PreferredSize(
         preferredSize: Size.fromHeight(80),
         child: AppBarCustom(title: 'PTA TEST'),
       ),
       drawer: MyDrawer(),
       body: Padding(
-        padding: const EdgeInsets.only(right: 25.0,left: 25.0),
+        padding: const EdgeInsets.only(right: 25.0, left: 25.0),
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
-            const Spacer(flex: 4,),
+            const Spacer(
+              flex: 3,
+            ),
             Expanded(
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter Patient Name',
-                  hintStyle: TextStyle(fontFamily: 'Rubik',fontWeight: FontWeight.w400,color: Color.fromRGBO(40, 51, 74, 1),),
+              flex: 3,
+              child: Card(
+                elevation: 0,
+                margin: EdgeInsets.symmetric(horizontal: 25),
 
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: kVeryLightGreyColor,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: const InputDecoration(
+                              hintText: 'Enter Patient Name',
+                              hintStyle: TextStyle(
+                                fontFamily: 'Rubik',
+                                fontWeight: FontWeight.w400,
+                                color: Color.fromRGBO(40, 51, 74, 1),
+                              ),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                    ],
                   ),
                 ),
+              ),
             ),
-
             const Spacer(),
-
             Expanded(
-              child: TextFormField(
-                decoration: const InputDecoration(
-                  hintText: 'Enter Patient Email',
-                  hintStyle: TextStyle(fontFamily: 'Rubik',fontWeight: FontWeight.w400,color: Color.fromRGBO(40, 51, 74, 1),),
+              flex: 3,
+              child: Card(
+                elevation: 0,
+                margin: EdgeInsets.symmetric(horizontal: 25),
 
+                shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20)),
+                color: kVeryLightGreyColor,
+                child: Container(
+                  padding: EdgeInsets.symmetric(horizontal: 20),
+                  child: Row(
+                    children: [
+                      Expanded(
+                        child: TextField(
+                          decoration: const InputDecoration(
+                              hintText: 'Enter Patient Email',
+                              hintStyle: TextStyle(
+                                fontFamily: 'Rubik',
+                                fontWeight: FontWeight.w400,
+                                color: Color.fromRGBO(40, 51, 74, 1),
+                              ),
+                              border: InputBorder.none),
+                        ),
+                      ),
+                    ],
+                  ),
                 ),
               ),
             ),
-
-            const Spacer(flex: 2,),
-
+            
+            const Spacer(
+              flex: 2,
+            ),
             Expanded(
+              flex: 2,
               child: GestureDetector(
                 onTap: () {
-                  Navigator.pushNamed(context, '/pureTone') ;
+                  Navigator.pushNamed(context, '/pureTone');
                 },
                 child: Container(
-                  height: 38,
-                  width: MediaQuery.of(context).size.width,
+                  margin: EdgeInsets.symmetric(horizontal: 25),
+                  height: 20,
+                  
                   color: const Color.fromRGBO(40, 51, 74, 1),
-                  child: const Center(child: Text('Register',style: TextStyle(fontFamily: 'Blinker',fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700))),
+                  child: const Center(
+                      child: Text('Register',
+                          style: TextStyle(
+                              fontFamily: 'Blinker',
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700))),
                 ),
               ),
             ),
-
-            const Spacer(flex: 3,),
-
+            const Spacer(
+              flex: 3,
+            ),
             Expanded(
               flex: 5,
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  SmallCard(imagePath: 'assets/icons/puretone.png', text: 'Pure Tone', onTap: (){}),
+                  SmallCard(
+                      imagePath: 'assets/icons/puretone.png',
+                      text: 'Pure Tone',
+                      onTap: () {}),
                   //SmallCard(imagePath: 'assets/icons/boneConduct.png', text: 'Bone Conduct', onTap: (){}),
                 ],
               ),
             ),
-
-            const Spacer(flex: 4,),
-
+            const Spacer(
+              flex: 7,
+            ),
           ],
         ),
       ),
     );
   }
 }
-
-
-
