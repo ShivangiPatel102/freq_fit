1025b9a4dac5aeafcf1cf74477dfdc05ed588bdf
ShivangiPatel102 <patelshivangi2002@gmail.com>
2024-02-21 18:41:24 +0530

    firebase auth updated

--- lib/screens/registration_screen.dart
+++ lib/screens/registration_screen.dart
@@ -2,25 +2,29 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class RegisterScreen extends StatelessWidget {
-
   late String email;
   late String password;
   late String firstName;
   late String lastName;
 
   final _auth = FirebaseAuth.instance;
-  final TextStyle sub = const TextStyle(fontWeight: FontWeight.w100,fontFamily: 'Blinker',fontSize: 20,);
+  final TextStyle sub = const TextStyle(
+    fontWeight: FontWeight.w100,
+    fontFamily: 'Blinker',
+    fontSize: 20,
+  );
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       body: Container(
-        padding: const EdgeInsetsDirectional.only(start: 25.0,end: 25.0),
+        padding: const EdgeInsetsDirectional.only(start: 25.0, end: 25.0),
         child: Column(
-
           crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            const Spacer(flex: 2,),
+            const Spacer(
+              flex: 2,
+            ),
             Expanded(
               flex: 3,
               child: Row(
@@ -31,7 +35,9 @@ class RegisterScreen extends StatelessWidget {
                     width: 80,
                     child: Column(
                       children: [
-                        Image.asset('assets/icons/appIcon.png',),
+                        Image.asset(
+                          'assets/icons/appIcon.png',
+                        ),
                       ],
                     ),
                   ),
@@ -40,72 +46,118 @@ class RegisterScreen extends StatelessWidget {
             ),
             Expanded(
               flex: 3,
-              child: const Text('Welcome', style: TextStyle(
-                fontFamily: 'Blinker',
-                fontWeight: FontWeight.w600,
-                fontSize: 50,
-                color: Color.fromRGBO(40, 51, 74,1),
-              ),),
+              child: const Text(
+                'Welcome',
+                style: TextStyle(
+                  fontFamily: 'Blinker',
+                  fontWeight: FontWeight.w600,
+                  fontSize: 50,
+                  color: Color.fromRGBO(40, 51, 74, 1),
+                ),
+              ),
             ),
-
             const Spacer(),
-
-             Expanded(child: TextField(decoration: InputDecoration(hintText: 'Full Name',hintStyle: sub ),)),
-
+            Expanded(
+                child: TextField(
+              decoration:
+                  InputDecoration(hintText: 'Full Name', hintStyle: sub),
+            )),
             const Spacer(),
-
-             Expanded(child: TextField(decoration: InputDecoration(hintText: 'Email ID',hintStyle: sub ),)),
-
+            Expanded(
+              child: TextField(
+                onChanged: (value) {
+                  email = value;
+                },
+                decoration:
+                    InputDecoration(hintText: 'Email ID', hintStyle: sub),
+              ),
+            ),
             const Spacer(),
-
-            Expanded(child: TextField(obscureText: true,decoration: InputDecoration(hintText: 'Password',hintStyle: sub ),)),
-
+            Expanded(
+                child: TextField(
+                  onChanged: (value) {
+                  password = value;
+                },
+              obscureText: true,
+              decoration: InputDecoration(hintText: 'Password', hintStyle: sub),
+            )),
             const Spacer(),
-
-            Expanded(child: TextField(decoration: InputDecoration(hintText: 'Hospital / Clinic name ',hintStyle: TextStyle(fontWeight: FontWeight.w100,fontFamily: 'Blinker',fontSize: 20,), ),)),
-
+            Expanded(
+                child: TextField(
+              decoration: InputDecoration(
+                hintText: 'Hospital / Clinic name ',
+                hintStyle: TextStyle(
+                  fontWeight: FontWeight.w100,
+                  fontFamily: 'Blinker',
+                  fontSize: 20,
+                ),
+              ),
+            )),
             const Spacer(),
-
-             Expanded(child: TextField(decoration: InputDecoration(hintText: 'Speciality ',hintStyle: sub ),)),
-
-            const Spacer(flex: 2,),
-
+            Expanded(
+                child: TextField(
+              decoration:
+                  InputDecoration(hintText: 'Speciality ', hintStyle: sub),
+            )),
+            const Spacer(
+              flex: 2,
+            ),
             Expanded(
               flex: 2,
               child: GestureDetector(
+                onTap: (){
+                  
+                },
                 child: Container(
                   padding: EdgeInsets.all(0),
                   width: MediaQuery.of(context).size.width,
                   color: const Color.fromRGBO(40, 51, 74, 1),
-                  child: const Center(child: Text('Register',style: TextStyle(fontFamily: 'Blinker',fontSize: 20,color: Colors.white,fontWeight: FontWeight.w700))),
+                  child: const Center(
+                      child: Text('Register',
+                          style: TextStyle(
+                              fontFamily: 'Blinker',
+                              fontSize: 20,
+                              color: Colors.white,
+                              fontWeight: FontWeight.w700))),
                 ),
               ),
             ),
-            const Spacer(flex: 1,),
-
+            const Spacer(
+              flex: 1,
+            ),
             Expanded(
-             
               child: Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.center,
                 children: [
-                  const Text("Have an Account?",style: TextStyle(fontFamily: 'Blinker',fontSize: 19.0,fontWeight: FontWeight.normal),),
-                  const SizedBox(width: 5.0,),
+                  const Text(
+                    "Have an Account?",
+                    style: TextStyle(
+                        fontFamily: 'Blinker',
+                        fontSize: 19.0,
+                        fontWeight: FontWeight.normal),
+                  ),
+                  const SizedBox(
+                    width: 5.0,
+                  ),
                   GestureDetector(
                     onTap: () {
-                      Navigator.pushNamed(context, '/login') ;                   },
-                    child: const Text('Login!',style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20),),
+                      Navigator.pushNamed(context, '/login');
+                    },
+                    child: const Text(
+                      'Login!',
+                      style:
+                          TextStyle(fontWeight: FontWeight.w700, fontSize: 20),
+                    ),
                   ),
                 ],
               ),
             ),
-
-            const Spacer(flex: 6,),
-
-
+            const Spacer(
+              flex: 6,
+            ),
           ],
         ),
-
       ),
     );
   }
